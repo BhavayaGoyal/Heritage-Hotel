0082016c4dddd10c7eb9399d8c3640f53d71b894
BhavayaGoyal <Bhavaya123>
2025-09-01 21:52:14 +0530

    Implement routing for main application pages in App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
-
+import Homepage from './homepage';
+import AboutUs from './AboutUs';
+import Contact from './Contact';
+import Dining from './Dining'
+import { Routes, Route } from "react-router-dom";
+import BookNow from './BookNow';
+import Blogs from './Blogs'
 import './App.css';
+import Login from './Login';
+import SignUp from './SignUp';
+import RoomsList from './RoomsList.jsx'
+import RoomDetail from './RoomDetail.jsx'
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <>
+    <Routes>
+      <Route path="/" element={<Homepage/>} />
+      <Route path='/about' element={<AboutUs/>}/>
+      <Route path='/contact' element={<Contact/>}/>
+      <Route path='/blogs' element={<Blogs/>}/>
+      <Route path='/dining' element={<Dining/>}/>
+      <Route path='/book-now' element={<BookNow/>}/>
+      <Route path='/login' element={<Login/>}/>
+      <Route path='/signup' element={<SignUp/>}/>
+      <Route path='/rooms' element={<RoomsList/>}/>
+      <Route path="/rooms/:slug" element={<RoomDetail />} />
+
+    </Routes>
+    </>
   );
 }
 
